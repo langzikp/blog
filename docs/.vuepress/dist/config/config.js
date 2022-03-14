f1d16630e73c937bc8161a5dd5c92d10d72593a2
niel <819750949@qq.com>
2022-03-14 13:49:58 +0800

    refactor:修改gitignore

---
+++ docs/.vuepress/dist/config/config.js
@@ -0,0 +1,27 @@
+var dataConfig =  {
+    infoList: [
+        {
+            title: "费曼学习法",
+            author: "lang nie",
+            date: "2019-02-01",
+            tag: "booklist",
+            url: "",
+            content: "通过向别人清楚的解说某一件事或是写一篇浅显易懂的教程文章，来确认自己是否真正弄懂了这件事。 可以说是在通过向别讲解过程中来对自身的 查缺补漏。"
+        },
+        {
+            title: "代码规范: 养成良好的编码习惯",
+            author: "lang nie",
+            date: "2022-02-01",
+            tag: "develop-norm",
+            url: "/develop-norm/engineer",
+            content: ""
+        },
+        {
+            title: "别乱提交代码了，看下大厂 Git 提交规范是怎么做的！",
+            author: "lang nie",
+            date: "2022-02-01",
+            tag: "develop-norm",
+            url: "/develop-norm/git",
+        }
+    ]
+}
